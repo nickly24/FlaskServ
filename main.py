3e426dc163f3c66792b8f2632fb747bbdf7a3b53
nickly24 <nikolaypribysh@mail.ru>
2024-11-30 11:33:41 +0300

    Test

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({'message': 'Hello, World!'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
