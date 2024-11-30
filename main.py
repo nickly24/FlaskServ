fd619bd98642104d93953d222c41fd36a1ee6e00
nickly24 <nikolaypribysh@mail.ru>
2024-11-30 11:43:10 +0300

    TEst new branch

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ CORS(app)
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({'message': 'Hello, World!'})
-
+@app.route('/api', methods=['GET'])
+def api():
+    return jsonify({'message': 'API is working'})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
